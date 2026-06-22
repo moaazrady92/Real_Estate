454d54a71e0d1b8d9e20dd924623b0726aff0aa8
moaazrady92 <moaaz.rady92@gmail.com>
2026-06-23 00:46:17 +0300

    Adding views,urls with some fixes to the settings

--- config/favorites/views.py
+++ config/favorites/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Favorite
+from .serializers import FavoriteSerializer
 
-# Create your views here.
+
+class FavoriteViewSet(viewsets.ModelViewSet):
+    serializer_class = FavoriteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Favorite.objects.filter(user=self.request.user)
--- config/listings/views.py
+++ config/listings/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from rest_framework.permissions import IsAuthenticatedOrReadOnly , SAFE_METHODS , BasePermission
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Listing
+from .serializers import ListingSerializer, ListingCreateSerializer
 
-# Create your views here.
+
+class IsOwnerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.owner == request.user
+
+
+class ListingViewSet(viewsets.ModelViewSet):
+    queryset = Listing.objects.filter(is_active=True)
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ["source"]
+    search_fields = ["title", "description", "address"]
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return ListingCreateSerializer
+        return ListingSerializer
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
